3e760c0cba7941a9c24105c5dedd3b3ce9bb57da
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-07 19:32:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
-const Client = require('./client/Client');
+const Client = require('./Client/Client');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
