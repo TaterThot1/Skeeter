4e60bb9f60e029a9d47997244bbcd3e1f585bec1
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-07 19:08:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const Client = require('./client/Client');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+console.log(client.commands);
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.once('ready', () => {
+    console.log('Succesfully Set Presence')
+    client.user.setActivity('Moderators', {type: "LISTENING"})
+
+});
+
+client.once('reconnecting', () => {
+	console.log('Reconnecting!');
+});
+
+client.once('disconnect', () => {
+	console.log('Disconnect!');
+});
+client.on('message', async message => {
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	const command = client.commands.get(commandName);
+
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	try {
+		if(commandName == "ban" || commandName == "userinfo") {
+			command.execute(message, client);
+		} else {
+			command.execute(message);
+		}
+	} catch (error) {
+		console.error(error);
+		message.reply('Sorry there was an error executing that command!');
+	}
+});
+
+
+client.login('Your Bot Token');
