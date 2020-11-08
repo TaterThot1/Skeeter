da89ba52486f34667a2eb094a601aa8a0d7eb651
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-08 16:55:16 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -10,7 +10,9 @@ module.exports = {
 			return message.reply('Please provide the number of messages to delete. (max 100)')
 		}
         
-
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+			return message.reply('You dont have the correct perms to use this command');
+		}
 		if (!deleteCount || deleteCount < 2 || deleteCount > 100)
 			return message.reply('Specify an amount, between 2 and 100, to delete.');
 
