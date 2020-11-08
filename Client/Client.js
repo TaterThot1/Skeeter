ca4869dc8d5436f6ffba27ca4b2cdbcbbba9be98
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-07 19:32:23 -0500

    Create Client.js

---
+++ Client/Client.js
@@ -0,0 +1,17 @@
+
+const { Client, Collection } = require('discord.js');
+
+module.exports = class extends Client {
+	constructor(config) {
+		super({
+			disableEveryone: true,
+			disabledEvents: ['TYPING_START'],
+		});
+
+		this.commands = new Collection();
+
+		this.queue = new Map();
+
+		this.config = config;
+	}
+};
