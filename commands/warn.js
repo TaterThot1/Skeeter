f070fcadfe46552a7ab58a7ca050d26547516112
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-13 00:19:16 -0500

    Create warn.js
    when giving reasons you cant separate text by spaces so just use - or _ for the time being

---
+++ commands/warn.js
@@ -0,0 +1,18 @@
+    const Discord = require('discord.js');
+module.exports = {
+	name: 'warn',
+	description: 'warn a user.',
+    execute(message, client) {
+    const { prefix} = require('../config.json');
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[2]);
+    if (!message.member.hasPermission("MANAGE\_MEMBER")) return message.reply("You can't use that command!")
+    if (!dUser) return message.channel.send("Can't find user!")
+    let dMessage = args.join(" ").slice(22);
+    if (dMessage.length < 1) return message.reply('what is the reason???')
+
+    dUser.send(`${dUser}, You have been warned for doing ${args[2]} in the server ${message.guild.name}`)
+
+    message.channel.send(`${dUser} has been warned for doing ${args[2]} :thumbsdown:`)
+	},
+};
