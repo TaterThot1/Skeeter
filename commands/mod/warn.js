aec78bc50944239d60be3e341c8fada05a14cf19
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-17 16:23:24 -0500

    Update warn.js

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -16,11 +16,11 @@ module.exports = class WarnCommand extends Command {
     let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[2]);
     if (!message.member.hasPermission("MANAGE\_MEMBER")) return message.reply("You can't use that command!")
     if (!dUser) return message.channel.send("Can't find user!")
-    let dMessage = args.join(" ").slice(22);
-    if (dMessage.length < 1) return message.reply('what is the reason???')
+    let reason = args.join(" ").slice(27);
+    if (reason.length < 1) return message.reply('what is the reason???')
 
-    dUser.send(`${dUser}, You have been warned for doing ${args[2]} in the server ${message.guild.name}`)
+    dUser.send(`${dUser}, You have been warned for${reason} in the server ${message.guild.name}`)
 
-    message.channel.send(`${dUser} has been warned for doing ${args[2]} :thumbsdown:`)
+    message.channel.send(`${dUser} has been warned for${reason} :thumbsdown:`)
 	}
 };
