1d2b4853db6129ddeb9c5311549705cbb7c955df
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-17 14:24:26 -0500

    Update and rename commands/kick.js to commands/mod/kick.js

--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -1,10 +1,18 @@
-const Client = require('../client/Client');
+const Client = require('../../client/Client');
 const Discord = require('discord.js');
-const getUserFromMention = require('../util/getUser');
-module.exports = {
-	name: 'kick',
-	description: 'kick a user.',
-    execute(message, client) {
+const getUserFromMention = require('../../util/getUser');
+    const { Command } = require('discord.js-commando');
+module.exports = class KickCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'kick',
+			aliases: ['kick'],
+			group: 'mod',
+			memberName: 'kick',
+			description: 'kick a user.',
+		});
+	}
+    run(message, client) {
        const Discord = require('discord.js')
        const tuser = message.guild.member(message.mentions.users.first());
        const user = message.mentions.users.first();
@@ -16,7 +24,7 @@ module.exports = {
 		}
 		return tuser.kick(user)
 			.then(() => message.reply(`Kicked ${user.username}`));
-	},
+	}
 };
   clientPerms: {
     guild: ['banMembers']
