d5c8c67a0abdfb7a8d36db1a4eac4df8b9e22dd3
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-17 14:39:16 -0500

    Update and rename commands/ban.js to commands/mod/ban.js

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -1,13 +1,22 @@
-const { getUserFromMention } = require('../util/getUser')
-
-module.exports = {
-	name: 'ban',
-	description: 'Ban a member',
-	execute(message, client) {
+const Client = require('../../client/Client');
+const Discord = require('discord.js');
+const getUserFromMention = require('../../util/getUser');
+    const { Command } = require('discord.js-commando');
+module.exports = class BanCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'ban',
+			aliases: ['ban'],
+			group: 'mod',
+			memberName: 'ban',
+			description: 'ban a user.',
+		});
+	}
+    run(message, client) {
 		const split = message.content.split(/ +/);
 		const args = split.slice(1);
 
-		const member = getUserFromMention(args[0], client);
+		const member = message.mentions.users.first();
 
 		if (!member) {
 			return message.reply('No member was mentioned.');
@@ -20,7 +29,7 @@ module.exports = {
 		return message.guild.members.ban(member)
 			.then(() => message.reply(`Banned ${member.username}`))
 			.catch(error => message.reply('Sorry, an error has unfortunately occured.'));
-	},
+    }
 };
   clientPerms: {
     guild: ['banMembers']
