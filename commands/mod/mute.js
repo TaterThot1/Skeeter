c2dd2b2b29a6dd458c2f26fb46886bccf009e449
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-27 13:02:50 -0500

    Update mute.js

--- commands/mod/mute.js
+++ commands/mod/mute.js
@@ -1,5 +1,5 @@
 const Client = require('../../client/Client');
-const Collection = require('../../node_modules/discord.js/src/util/Collection.js')
+const Collection = require('../../node_modules/discord.js/src/util/Collection.js');
 const Discord = require('discord.js');
 const getUserFromMention = require('../../util/getUser');
 const { Command } = require('discord.js-commando');
@@ -13,35 +13,29 @@ module.exports = class MuteCommand extends Command {
 			description: 'Mute user.',
 		});
 	}
-    async run(message, client) {
-     	  const { prefix} = require('../../config.json');
-          const log = message.guild.channels.cache.find(ch => ch.name === 'Log_Channel_Name');
-	  // Replace 'Log_channel_Name' to the name of you log channel.
+    run(message, client) {
+        const { prefix, logName, roleName1, roleName2, roleName3, roleName4, } = require('../../config.json');
+          const log = message.guild.channels.cache.find(ch => ch.name === logName);
           let member = message.mentions.members.first();
-          let roleName1 = 'Muted';
-                          //If yout mute role is named something else put it here
           let mute = message.guild.roles.cache.find(x => x.name === roleName1);
-          let roleName2 = 'Main_Role1';
-                          //Replace Main_Role1 with roles that have perms in chats
           let remove1 = message.guild.roles.cache.find(x => x.name === roleName2);
-          let roleName3 = 'Main_Role2';
-                            //If you have any other main roles rember to list them along with the line below
           let remove2 = message.guild.roles.cache.find(x => x.name === roleName3);
+          let remove3 = message.guild.roles.cache.find(x => x.name === roleName4);
 		if (!message.member.hasPermission("MANAGE\_MEMBERS")) {
 			return message.reply('I am unable to mute this user.');
 		}
           const args = message.content.slice(prefix.length).trim().split(/ +/);
        	  let reason = args.join(" ").slice(27);
-     		if (reason.length < 1) return message.reply('what is the reason???')
+     		if (reason.length < 1) return message.reply('what is the reason???');
 
           let ismuted = member.roles.cache.some(role => role.name === roleName1);
        		if (ismuted) return message.reply(`sorry but ${member} is already muted`);
         		member.roles.add(mute);
         		member.roles.remove(remove1);
         		member.roles.remove(remove2);
-        		//each time you add a main role remeber to add another member.roles.remove line to remove them
+        		member.roles.remove(remove3);
           return message.reply(`muted ${member}`)
-		.then(() => log.send({embed: {color: '#FF0000', title: 'Mute Log', description: `${member} has been muted for ${reason}.`}}))
+		.then(() => log.send({embed: {color: '#FF0000', title: 'Mute Log', description: `${member} has been muted for ${reason}.`}}));
     }
 };
 //if @everyone has the perm to type or speak the command wont work
