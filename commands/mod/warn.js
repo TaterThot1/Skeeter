22bef6154b5669472c11bf695d80874dffe02c6c
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-27 13:08:46 -0500

    Update warn.js

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -11,16 +11,20 @@ module.exports = class WarnCommand extends Command {
 		});
 	}
     run(message, client) {
-    const { prefix} = require('../../config.json');
+    const { prefix,
+        logName,
+    } = require('../../config.json');
+    const log = message.guild.channels.cache.find(ch => ch.name === logName);
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[2]);
-    if (!message.member.hasPermission("MANAGE\_MEMBER")) return message.reply("You can't use that command!")
-    if (!dUser) return message.channel.send("Can't find user!")
+    if (!message.member.hasPermission("MANAGE\_MEMBER")) return message.reply("You can't use that command!");
+    if (!dUser) return message.channel.send("Can't find user!");
     let reason = args.join(" ").slice(27);
-    if (reason.length < 1) return message.reply('what is the reason???')
+    if (reason.length < 1) return message.reply('what is the reason???');
 
-    dUser.send(`${dUser}, You have been warned for${reason} in the server ${message.guild.name}`)
+    dUser.send(`${dUser}, You have been warned for${reason} in the server ${message.guild.name}`);
 
-    message.channel.send(`${dUser} has been warned for${reason} :thumbsdown:`)
+    return message.channel.send(`${dUser} has been warned for${reason} :thumbsdown:`)
+ 			.then(() => log.send({embed: {color: '#FF0000', title: 'Warn Log', description: `${dUser} has been warned for ${reason}.`}}));
 	}
 };
