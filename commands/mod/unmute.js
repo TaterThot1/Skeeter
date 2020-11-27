f1b8ed4d87266375523d9b6a83464cc1aa1f747e
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-11-27 13:03:39 -0500

    Update unmute.js

--- commands/mod/unmute.js
+++ commands/mod/unmute.js
@@ -1,5 +1,5 @@
 const Client = require('../../client/Client');
-const Collection = require('../../node_modules/discord.js/src/util/Collection.js')
+const Collection = require('../../node_modules/discord.js/src/util/Collection.js');
 const Discord = require('discord.js');
 const getUserFromMention = require('../../util/getUser');
 const { Command } = require('discord.js-commando');
@@ -13,16 +13,15 @@ module.exports = class UnmuteCommand extends Command {
 			description: 'Unmute user.',
 		});
 	}
-    async run(message, client) {
-     	    const { prefix} = require('../../config.json');
-          const log = message.guild.channels.cache.find(ch => ch.name === 'Log_Channel_Name');
-	              // Replace 'Log_channel_Name' to the name of you log channel.
+    run(message, client) {
+        const { prefix,
+        logName,
+        roleName1,
+        roleName2,
+         } = require('../../config.json');
+           const log = message.guild.channels.cache.find(ch => ch.name === logName);
           let member = message.mentions.members.first();
-          let roleName1 = 'Muted';
-                      // If you mute role is named differently replace 'Muted' with it.
           let mute = message.guild.roles.cache.find(x => x.name === roleName1);
-          let roleName2 = 'Main_Role';
-                      // Replace 'Main_Role' with the name of your server's main role.
           let add1 = message.guild.roles.cache.find(x => x.name === roleName2);
 		            if (!message.member.hasPermission("MANAGE\_MEMBERS")) {
 			            return message.reply('I am unable to unmute this user.');
@@ -36,6 +35,6 @@ module.exports = class UnmuteCommand extends Command {
                       member.roles.add(add1);
                       member.roles.remove(mute);
           return message.reply(`unmuted ${member}.`)
-	            .then(() => log.send({embed: {color: '#FF0000', title: 'Mute Log', description: `${member} has been unmuted for ${reason}.`}}))
+	            .then(() => log.send({embed: {color: '#FF0000', title: 'Mute Log', description: `${member} has been unmuted for ${reason}.`}}));
     }
 };
