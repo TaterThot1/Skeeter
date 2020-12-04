95eace27795104e4557f9a56c21f2230f1969f0b
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2020-12-04 01:55:57 -0500

    Create serverinfo.js

---
+++ commands/misc/serverinfo.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const { Command } = require('discord.js-commando');
+module.exports = class ServerInfoCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'serverinfo',
+			aliases: ['serverinfo'],
+			group: 'misc',
+			memberName: 'serverinfo',
+			description: 'info about the server.',
+		});
+	}
+    async run (message) {
+       const { guild, channel } = message;
+       let rolemap = guild.roles.cache
+            .sort((a, b) => b.position - a.position)
+            .map(r => r)
+            .join("");
+            if (rolemap.length > 1024) rolemap = "To many roles to display";
+            if (!rolemap) rolemap = "No roles";
+       const Embed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Server Info')
+	.setAuthor(`Server Acromyn- ${message.guild.nameAcronym}`)
+	.setDescription(`${message.guild.name}`)
+	.setThumbnail(`${message.guild.iconURL({ format: "png", dynamic: true })}`)
+    .addFields(
+		{ name: 'Member Count', value: `${message.guild.memberCount}` },
+		{ name: 'Creation Date', value: `${message.guild.createdAt}` },
+        { name: 'Boost Level', value: `${message.guild.premiumTier}`},
+		{ name: 'Guild Id', value: `${message.guild.id}`},
+		{ name: 'Roles', value: `${rolemap}`}
+		)
+	.setTimestamp()
+	.setFooter('Info');
+
+message.channel.send(Embed);
+	}
+};
