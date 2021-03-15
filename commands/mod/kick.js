656738855b8a59e79f8748b8b31e74d79f385a92
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2021-03-15 13:27:38 -0400

    Update kick.js

--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -1,7 +1,7 @@
 const Client = require('../../client/Client');
 const Discord = require('discord.js');
 const getUserFromMention = require('../../util/getUser');
-    const { Command } = require('discord.js-commando');
+const { Command } = require('discord.js-commando');
 module.exports = class KickCommand extends Command {
 	constructor(client) {
 		super(client, {
@@ -14,7 +14,7 @@ module.exports = class KickCommand extends Command {
 	}
     run(message, client) {
        const { prefix,
-           logName,
+               logName,
             } = require('../../config.json');
        const log = message.guild.channels.cache.find(ch => ch.name === logName);
        const Discord = require('discord.js');
@@ -23,13 +23,14 @@ module.exports = class KickCommand extends Command {
        if (reason.length < 1) return message.reply('what is the reason???');
        const tuser = message.guild.member(message.mentions.users.first());
        const user = message.mentions.users.first();
+       const mod = message.member.user;
         if (!user) {
 			return message.reply('No member was mentioned.');
 		}
         if (!message.member.hasPermission("kickMembers")) {
-			return message.reply('You dont have the correct perms to use this command');
+			return message.reply('You dont have the correct permissions to use this command');
 		}
 		return tuser.kick(reason)
-			.then(() => log.send({embed: {color: '#FF0000', title: 'Kick Log', description: `${user} has been kicked.`}}));
+			.then(() => log.send({embed: {color: '#FF0000', title: 'Kick Log', description: `${user} has been kicked by ${mod}.`}}));
 	}
 };
