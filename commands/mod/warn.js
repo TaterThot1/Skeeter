17cec0e7d10798b215dcb5f9484d4161d62da072
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2021-03-15 12:05:38 -0400

    Update warn.js

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -1,30 +1,31 @@
-    const Discord = require('discord.js');
+   const Discord = require('discord.js');
     const { Command } = require('discord.js-commando');
 module.exports = class WarnCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'warn',
-			aliases: ['warn'],
-			group: 'mod',
-			memberName: 'warn',
-			description: 'warn a user.',
-		});
-	}
+  constructor(client) {
+    super(client, {
+      name: 'warn',
+      aliases: ['warn'],
+      group: 'mod',
+      memberName: 'warn',
+      description: 'warn a user.',
+    });
+  }
     run(message, client) {
     const { prefix,
         logName,
     } = require('../../config.json');
     const log = message.guild.channels.cache.find(ch => ch.name === logName);
     const args = message.content.slice(prefix.length).trim().split(/ +/);
-    let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[2]);
+    let dUser = message.guild.member(message.mentions.users.first()) || message.channel.send('No one was mentioned')
+    let mod = message.member.user
     if (!message.member.hasPermission("MANAGE\_MEMBER")) return message.reply("You can't use that command!");
     if (!dUser) return message.channel.send("Can't find user!");
     let reason = args.join(" ").slice(27);
     if (reason.length < 1) return message.reply('what is the reason???');
 
-    dUser.send(`${dUser}, You have been warned for${reason} in the server ${message.guild.name}`);
+    dUser.send(`${mod} has warned you for ${reason} in the server ${message.guild.name}`); 
 
-    return message.channel.send(`${dUser} has been warned for${reason} :thumbsdown:`)
- 			.then(() => log.send({embed: {color: '#FF0000', title: 'Warn Log', description: `${dUser} has been warned for ${reason}.`}}));
-	}
+    return message.channel.send(`${dUser} has been warned for ${reason} by${mod} :thumbsdown:`)
+       .then(() => log.send({embed: {color: '#FF0000', title: 'Warn Log', description: `${dUser} has been warned by ${mod} for ${reason}.`}}));
+  }
 };
