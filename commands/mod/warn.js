ee0ee5cc5cb4483128dcb2186662cc68ea570233
TaterThot1 <65298513+TaterThot1@users.noreply.github.com>
2021-03-15 13:23:09 -0400

    Update warn.js

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -1,5 +1,5 @@
    const Discord = require('discord.js');
-    const { Command } = require('discord.js-commando');
+   const { Command } = require('discord.js-commando');
 module.exports = class WarnCommand extends Command {
   constructor(client) {
     super(client, {
@@ -12,8 +12,8 @@ module.exports = class WarnCommand extends Command {
   }
     run(message, client) {
     const { prefix,
-        logName,
-    } = require('../../config.json');
+            logName,
+          } = require('../../config.json');
     const log = message.guild.channels.cache.find(ch => ch.name === logName);
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     let dUser = message.guild.member(message.mentions.users.first()) || message.channel.send('No one was mentioned')
@@ -22,9 +22,7 @@ module.exports = class WarnCommand extends Command {
     if (!dUser) return message.channel.send("Can't find user!");
     let reason = args.join(" ").slice(27);
     if (reason.length < 1) return message.reply('what is the reason???');
-
     dUser.send(`${mod} has warned you for ${reason} in the server ${message.guild.name}`); 
-
     return message.channel.send(`${dUser} has been warned for ${reason} by${mod} :thumbsdown:`)
        .then(() => log.send({embed: {color: '#FF0000', title: 'Warn Log', description: `${dUser} has been warned by ${mod} for ${reason}.`}}));
   }
